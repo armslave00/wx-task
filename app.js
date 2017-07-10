494450822dddaaf09f6bd54d6d644323b067d8aa
杨洋 <armslave00@gmail.com>
2017-07-10 11:53:21 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var request = require('request');
+// var sleep = require('sleep');
+var winston = require('winston');
+var moment = require('moment');
+
+// var SLEEP_TIME = 60;
+var TASK_PERIOD = 3600 * 1000 * 1.5;
+
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({ filename: 'log/info.log' })
+    ]
+  });
+
+// var logInfo = function (info) {
+//   var time = moment().format();
+//   logger.info(time + '# ' + info);
+// }
+logger.info('Task Start');
+
+var timer = setInterval(function () {
+    // logInfo('Tick');
+    logger.info('Make Request');
+    request.post('http://121.199.52.161:18083/weixin/auth/jsparam', {form:{url:'https://weixin.idongjia.cn/h5-item/334249'}}, function (error, response, body) {
+        if (error) {
+          logger.info('error:' + error); // Print the error if one occurred
+        }
+        logger.info('statusCode:' + (response && response.statusCode)); // Print the response status code if a response was received
+        logger.info('body:' + body); // Print the HTML for the Google homepage.
+    });
+}, TASK_PERIOD);
+
+// sleep.sleep(SLEEP_TIME);
